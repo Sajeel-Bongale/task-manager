b2ff5f3b0c1849ab5ee5a25af65f2faa0b2575a5
Sajeel-Bongale <sajeelbongale@gmail.com>
2021-09-27 17:51:13 +0530

    Creates schema for Task

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://common_user:password_user@task-manager-cluster.o3iq5.mongodb.net/task-manager?retryWrites=true&w=majority"
-
 const connectDB = (url) => {
     return mongoose.connect(url, {
         useNewUrlParser: true,
